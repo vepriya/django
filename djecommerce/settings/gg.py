929d0e6920ed15d5b1e77cfb96b4b2a385f4a992
vepriya <46642471+vepriya@users.noreply.github.com>
2020-07-07 13:35:00 +0530

    Add files via upload

---
+++ djecommerce/settings/gg.py
@@ -0,0 +1,6 @@
+import os
+#from decouple import config
+z=C:\\Users\PC\Desktop\django-ecommerce\djecommerce\settings\gg.py
+BASE_DIR = os.path.dirname(os.path.dirname(
+    os.path.dirname(os.path.abspath(z))))
+print(BASE_DIR)
